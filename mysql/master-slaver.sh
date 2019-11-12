a40f280b714c293e4710507587924079d60ec116
guazike <lrh992@sina.com>
2019-11-12 11:36:45 +0800

    fix

--- mysql/master-slaver.sh
+++ mysql/master-slaver.sh
@@ -20,10 +20,15 @@ sync_binlog=1  #开启binlog日志同步功能
 -- GRANT REPLICATION SLAVE ON *.* to 'slave245'@'172.18.171.245' identified by 'WCY7EWQBiKHDvFgHcFgeqqvA';#在主Mysql上建立slave245帐户并授权为Slave:
 -- flush privileges;
 
-3、锁库
-flush tables with read lock;
-#从库完成启动后解锁主库
-unlock tables;
+3、锁库再导出数据
+flush tables with read lock;
+show master status;#查看并记下主库当前bin-log文件和操作位置，从库导入数据后从该位置同步
+#导出数据
+mysqldump -u账号 -p密码 --databases 要导出的数据库名 >/tmp/testA.sql
+unlock tables;
+
+4、拷贝数据到从服务器
+scp /tmp/testA.sql 10.0.0.104:/tmp
 
 从：
 1、修改配置
@@ -33,16 +38,23 @@ log-bin=mysql-bin
 binlog_format=MIXED
 innodb_flush_log_at_trx_commit=2
 sync_binlog=1
-read_only = ON
+replicate-do-db=some_schema#要同步的数据库
+replicate-ignore-db=sys#不想同步的数据库
+#read_only = ON
 
-2、修改mysql数据目录下的auto.cnf
+2、导入主库的同步前数据
+mysql>drop table 将导入的数据库名 #先删除同名数据库
+mysql -uroot -pyangcan </tmp/testA.sql #导入数据
+systemctl restart mysql.service #重启数据库
+
+3、修改mysql数据目录下的auto.cnf
 cat >/var/lib/mysql/auto.cn
 [auto]
 server-uuid=772a84e9-2e79-11e8-8548-00163e0a7独立尾号
 
-3、mysql客户端中设置从数据库的master
+4、mysql客户端中设置从数据库的master
 #master_log_file文件为主库中 show master status 中的File，master_log_post为主库中Postion的值
--- change master to master_host='172.18.196.183',master_user='slave245',master_password='WCY7EWQBiKHDvFgHcFgeqqvA',master_log_file='log-bin.000002',master_log_pos=2248;
+-- change master to master_host='172.18.196.183',master_user='slave245',master_password='WCY7EWQBiKHDvFgHcFgeqqvA',master_log_file='log-bin.000002',master_log_pos=主库导出数据前的操作位置;
 -- #change master to master_connect_retry=50;# 连接断开时，重新连接超时时间
 -- start slave;#启动slave的io和sql线程
 -- stop slave;
